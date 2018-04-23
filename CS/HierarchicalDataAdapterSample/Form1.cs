6625742ad5417ba0f7f41818b4bf3507d1f8f0f0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HierarchicalDataAdapterSample/Form1.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.XtraTreeMap;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HierarchicalDataAdapterSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            CreateTreeMapDataAdapter();
+            CreateTreeMapColorizer();
+        }
+
+        #region #CreateDataAdapter
+        void CreateTreeMapDataAdapter() {
+            TreeMapHierarchicalDataAdapter dataAdapter = new TreeMapHierarchicalDataAdapter();
+            dataAdapter.DataSource = CreateStatistics();
+            
+            // Fill the Mappings collection using mappings specifying 
+            // how to convert data objects to tree map items.
+            dataAdapter.Mappings.Add(new TreeMapHierarchicalDataMapping {
+                Type = typeof(CountryStatistics),
+                LabelDataMember = "Name",
+                ChildrenDataMember = "EnergyStatistics"
+            });
+            dataAdapter.Mappings.Add(new TreeMapHierarchicalDataMapping {
+                Type = typeof(EnergyInfo),
+                LabelDataMember = "Type",
+                ValueDataMember = "Value"
+            });
+
+            treeMap.DataAdapter = dataAdapter;
+        }
+        #endregion #CreateDataAdapter
+
+        #region #GradientColorizer
+        void CreateTreeMapColorizer() {
+            treeMap.Colorizer = new TreeMapGradientColorizer {
+                StartColor = Color.Black,
+                EndColor = Color.White
+            };
+        }
+        #endregion #GradientColorizer
+
+        List<CountryStatistics> CreateStatistics() {
+            List<CountryStatistics> statistics = new List<CountryStatistics>();
+            XDocument doc = XDocument.Load("..\\..\\Data\\EnergyStatistic.xml");
+            foreach(XElement xCountry in doc.Element("ArrayOfEnergyStatistic").Elements("CountryEnergyInfo")) {
+                CountryStatistics countryStatistics = new CountryStatistics();
+                countryStatistics.Name = xCountry.Attribute("Country").Value;
+                foreach(XElement xEnergyInfo in xCountry.Elements("EnergyStatistic")) {
+                    countryStatistics.EnergyStatistics.Add(new EnergyInfo {
+                        Type = xEnergyInfo.Attribute("TypeName").Value,
+                        Value = Convert.ToDouble(xEnergyInfo.Attribute("Value").Value)
+                    });
+                }
+                statistics.Add(countryStatistics);
+            }
+            return statistics;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: provide hierarchical data to TreeMap
+
+
+This example demonstrates how to visualize hierarchical data using TreeMap and color it using Gradient Colorizer.
+
+
+<h3>Description</h3>
+
+<p>To provide hierarchical data to TreeMap, create a&nbsp;<strong>TreeMapHierarchicalDataAdapter</strong>&nbsp;object and assign it to the&nbsp;<strong>TreeMapControl.DataAdapter</strong>&nbsp;property. Then, specify the adapter's data source object using the&nbsp;<strong>DataSource&nbsp;</strong>property. To configure how hierarchical data should be converted to tree map items, use&nbsp;<strong>TreeMapHierarchicalDataMapping</strong>&nbsp;objects. The LabelDataMember property allows you to specify label data member, the ValueDataMember property -&nbsp;a value data member, the&nbsp;<strong>ChildrenDataMember&nbsp;</strong>property&nbsp;-&nbsp;a data member containing children items and the&nbsp;<strong>Type&nbsp;</strong>property - a type of items on current nesting level.<br><br>Finally, to color each tree map items depending on its value&nbsp;colors, use the&nbsp;<strong>TreeMapGradientColoriezer&nbsp;</strong>class. The&nbsp;<strong>StartColor</strong>&nbsp;property allows you to specify color for an item with the maximum value, and&nbsp;the&nbsp;<strong>EndColor&nbsp;</strong>property&nbsp;allows you to specify color for an item with the&nbsp;minimum&nbsp;value.</p>
+
+<br/>
+
+
